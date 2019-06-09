8b78b811208eb776faf0a3dd0898103a1c1cf3a9
sopderbeck <sopderbeck@est.frba.utn.edu.ar>
2019-06-08 22:09:55 -0300

    firt-commit in development

--- functions/Menu_controller.cpp
+++ functions/Menu_controller.cpp
@@ -1,14 +1,23 @@
-int menu (){
+//
+// Created by sopderbeck on 5/6/2019.
+//
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <fstream>
+#include <stdlib.h>
 
+using namespace std;
+
+int menu (){
+    system ("CLS");
     char tecla;
     int posicion = 1;
     int temporizador = 100;
 
-    segundo_menu ();
-
 
     while (true){
-        ocultar_cursor ();
         Sleep (15);
         if (posicion == 1){
             gotoxy (58 , 22);
@@ -30,15 +39,9 @@ int menu (){
             gotoxy (58 , 30);
             cout << (char) 175;
         }
-        else if (posicion == 6){
-            gotoxy (58 , 32);
+        if (posicion == 0){
+            gotoxy (58 , 20);
             cout << (char) 175;
-        }
-        if (hay_partida == true){
-            if (posicion == 0){
-                gotoxy (58 , 20);
-                cout << (char) 175;
-            }
         }
         if (kbhit()){
             if (posicion == 1){
@@ -61,72 +64,33 @@ int menu (){
                 gotoxy (58 , 30);
                 cout << " ";
             }
-            else if (posicion == 6){
-                gotoxy (58 , 32);
+            if (posicion == 0){
+                gotoxy (58 , 20);
                 cout << " ";
             }
-            if (hay_partida == true){
-                if (posicion == 0){
-                    gotoxy (58 , 20);
-                    cout << " ";
-                }
-            }
             tecla = getch();
-            if ((tecla == 'w') || (tecla == (int) 72) || (tecla == 'W')){
-                if (hay_partida == true){
-                    if (posicion == 0){
-                        posicion = 6;
-                    }
-                    else {
-                        posicion --;
-                    }
-                }
-                else if (posicion == 1){
-                    posicion = 6;
-                }
-                else {
-                    posicion --;
-                }
-            }
-            else if ((tecla == 's') || (tecla == (int) 80) || (tecla == 'S')){
-                if (hay_partida == true){
-                    if (posicion == 6){
-                        posicion = 0;
-                    }
-                    else {
-                        posicion ++;
-                    }
-                }
-                else if (posicion == 6){
-                    posicion = 1;
-                }
-                else {
-                    posicion ++;
-                }
-            }
+            changePosition(tecla);
+
             else if (tecla == (int) 13){
                 if (posicion == 1){
-                    crear_partida (lenguaje , mis_limites , mi_salud , mi_nave , mi_matriz, puntaje , mi_Partida);
+                    //funcion 1
                 }
                 else if (posicion == 2){
-                    hay_partida = true;
-                    abrir_partida (lenguaje ,mis_limites ,mi_salud ,mi_nave ,mi_matriz , puntaje, mi_Partida , hay_partida );
+                    //funcion 2
                 }
                 else if (posicion == 3){
-                    guardar_Partida (mi_Partida , lenguaje , mi_salud , mi_nave , mi_matriz, puntaje);
+                    //funcion 3
                 }
                 else if (posicion == 4){
-                    informacion_basica (lenguaje , hay_partida);
+                    //funcion 4
                 }
                 else if (posicion == 5){
-                    partidas (lenguaje , mis_limites , mi_salud , mi_nave , mi_matriz , puntaje , mi_Partida , hay_partida);
-                }
-                else if (posicion == 6){
-                    tutorial (lenguaje , mis_limites , mi_salud , mi_nave , mi_matriz, puntaje , mi_Partida);
+                    //funcion 5
                 }
                 else if (posicion == 0){
-                    system ("CLS");
-                    return 0;
+                    //funcion 0
+                    //system ("CLS");
+                    //return 0;
                 }
             }
             if (posicion == 1){
@@ -149,15 +113,9 @@ int menu (){
                 gotoxy (58 , 30);
                 cout << (char) 175;
             }
-            else if (posicion == 6){
-                gotoxy (58 , 32);
+            else if (posicion == 0){
+                gotoxy (58 , 20);
                 cout << (char) 175;
-            }
-            if (hay_partida == true){
-                if (posicion == 0){
-                    gotoxy (58 , 20);
-                    cout << (char) 175;
-                }
             }
             temporizador = 100;
         }
@@ -182,15 +140,9 @@ int menu (){
                 gotoxy (58 , 30);
                 cout << " ";
             }
-            else if (posicion == 6){
-                gotoxy (58 , 32);
+            if (posicion == 0){
+                gotoxy (58 , 20);
                 cout << " ";
-            }
-            if (hay_partida == true){
-                if (posicion == 0){
-                    gotoxy (58 , 20);
-                    cout << " ";
-                }
             }
             temporizador = 100;
             Sleep (500);
@@ -200,6 +152,7 @@ int menu (){
         }
     }
 }
+
 void segundo_menu(){
 
     gotoxy (0 , 5);
@@ -217,24 +170,31 @@ void segundo_menu(){
     Sleep(50);
     cout << "\t    ########       ##########       ##########     ##########       #########       #########       ###     ###    \n";
 
+    gotoxy (60 , 22);
+    cout << "   Nombre de funcion ";
+    Sleep(50);
+    gotoxy (60 , 24);
+    cout << "   Nombre de funcion ";
+    Sleep(50);
+    gotoxy (60 , 26);
+    cout << "  Nombre de funcion";
+    Sleep(50);
+    gotoxy (60 , 28);
+    cout << " Nombre de funcion";
+    Sleep(50);
+    gotoxy (60 , 30);
+    cout << " Nombre de funcion";
+    Sleep(50);
 
+}
 
-        gotoxy (60 , 22);
-        cout << "   CREAR PARTIDA";
-        Sleep(50);
-        gotoxy (60 , 24);
-        cout << "   ABRIR PARTIDA";
-        Sleep(50);
-        gotoxy (60 , 26);
-        cout << "  GUARDAR PARTIDA";
-        Sleep(50);
-        gotoxy (60 , 28);
-        cout << " INFORMACION BASICA";
-        Sleep(50);
-        gotoxy (60 , 30);
-        cout << " PARTIDAS GUARDADAS";
-        Sleep(50);
-        gotoxy (60 , 32);
-        cout << "     TUTORIAL";
-
+void changePosition(string tecla, int &posicion){
+    if ((tecla == 'w') || (tecla == (int) 72) || (tecla == 'W')){
+        if (posicion == 0) posicion = 5;
+        else posicion --;
+    }
+    else if ((tecla == 's') || (tecla == (int) 80) || (tecla == 'S')){
+        if (posicion == 5) posicion = 0;
+        else posicion ++;
+    }
 }
