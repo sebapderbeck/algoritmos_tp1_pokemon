33f474f433fe92de95ba58475301ef1c6a417934
Sopderbeck <sebastian.opderbeck@wolox.com.ar>
2019-07-21 12:16:03 -0300

    comment by terminal

---
+++ cmake-build-debug/CMakeFiles/algoritmos_tp1_pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algoritmos_tp1_pokemon.dir/main.cpp.obj"
+  "algoritmos_tp1_pokemon.pdb"
+  "algoritmos_tp1_pokemon.exe"
+  "algoritmos_tp1_pokemon.exe.manifest"
+  "libalgoritmos_tp1_pokemon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algoritmos_tp1_pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
